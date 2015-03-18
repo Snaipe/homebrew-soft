ddbe49aa8b4cdf2db2328d6dff0fa3af8c573f9d
Snaipe <franklinmathieu@gmail.com>
2015-03-18 16:58:48 +0100

    Fixed gcc not being picked up by autotools in libcsptr; added --devel switch support

--- Formula/libcsptr.rb
+++ Formula/libcsptr.rb
@@ -4,13 +4,18 @@ class Libcsptr < Formula
   homepage "https://github.com/Snaipe/c-smart-pointers/"
   url "https://github.com/Snaipe/c-smart-pointers.git", :tag => "v1.0"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool"  => :build
-  depends_on "gcc"      => :build
+  depends_on "autoconf"                => :build
+  depends_on "automake"                => :build
+  depends_on "libtool"                 => :build
+  depends_on "homebrew/versions/gcc49" => :build
+
+  devel do
+    url "https://github.com/Snaipe/c-smart-pointers.git"
+  end
 
   def install
-    system "./autogen.sh && ./configure --prefix=#{prefix}"
+    system "./autogen.sh"
+    system "./configure CC='gcc-4.9' --prefix=#{prefix}"
     system "make install"
   end
 end
