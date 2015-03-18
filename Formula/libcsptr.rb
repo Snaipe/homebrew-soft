88c8208dfb35dca64cf506b9c5a3fedcdc4c54b7
Snaipe <franklinmathieu@gmail.com>
2015-03-18 20:18:51 +0100

    Fixed the formula for libcsptr to force GCC usage

--- Formula/libcsptr.rb
+++ Formula/libcsptr.rb
@@ -3,15 +3,13 @@ require 'formula'
 class Libcsptr < Formula
   homepage "https://github.com/Snaipe/c-smart-pointers/"
   url "https://github.com/Snaipe/c-smart-pointers.git", :tag => "v1.0"
+  head "https://github.com/Snaipe/c-smart-pointers.git"
 
   depends_on "autoconf"                => :build
   depends_on "automake"                => :build
   depends_on "libtool"                 => :build
   depends_on "homebrew/versions/gcc49" => :build
-
-  devel do
-    url "https://github.com/Snaipe/c-smart-pointers.git"
-  end
+  fails_with :clang
 
   def install
     system "./autogen.sh"
