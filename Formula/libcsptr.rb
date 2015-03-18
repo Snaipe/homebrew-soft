15b4356577e106e7476d0ca2bafc650fa5c54012
Snaipe <franklinmathieu@gmail.com>
2015-03-18 20:24:54 +0100

    Removed gcc version dependency

--- Formula/libcsptr.rb
+++ Formula/libcsptr.rb
@@ -5,15 +5,15 @@ class Libcsptr < Formula
   url "https://github.com/Snaipe/c-smart-pointers.git", :tag => "v1.0"
   head "https://github.com/Snaipe/c-smart-pointers.git"
 
-  depends_on "autoconf"                => :build
-  depends_on "automake"                => :build
-  depends_on "libtool"                 => :build
-  depends_on "homebrew/versions/gcc49" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool"  => :build
+  depends_on "gcc"      => :build
   fails_with :clang
 
   def install
     system "./autogen.sh"
-    system "./configure CC='gcc-4.9' --prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 end
