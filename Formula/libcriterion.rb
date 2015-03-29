328a1fd7b813d0d6ba100dd0adcb07e3fae3d794
Snaipe <franklinmathieu@gmail.com>
2015-03-29 22:56:15 +0200

    Added criterion formula

---
+++ Formula/libcriterion.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+$repo = "https://github.com/Snaipe/Criterion.git"
+
+class Libcriterion < Formula
+  homepage "https://snaipe.github.io/Criterion"
+  url  $repo, :tag => "v1.1.0"
+  head $repo
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool"  => :build
+  depends_on "gcc"      => :build
+  fails_with :clang
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
