4022fe488fdf716a4c6efb9121a5566caa6954bb
Snaipe <franklinmathieu@gmail.com>
2015-09-22 00:25:55 +0200

    [libcriterion] Updated to v2.1.0

--- Formula/libcriterion.rb
+++ Formula/libcriterion.rb
@@ -4,18 +4,18 @@ $repo = "https://github.com/Snaipe/Criterion.git"
 
 class Libcriterion < Formula
   homepage "https://snaipe.github.io/Criterion"
-  url  $repo, :tag => "v1.2.1"
+  url  $repo, :tag => "v2.1.0"
   head $repo
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool"  => :build
-  depends_on "gcc"      => :build
+  depends_on "cmake" => :build
+  depends_on "gcc"   => :build
   fails_with :clang
 
   def install
-    system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}"
+    system "cmake",
+        "-DCMAKE_BUILD_TYPE=RelWithDebInfo",
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+        "."
     system "make install"
   end
 end
