ec279b256442577f7372eede1b9f6b3877f75ca7
Snaipe <franklinmathieu@gmail.com>
2015-09-22 13:04:30 +0200

    [libcsptr] updated to 2.0.4

--- Formula/libcsptr.rb
+++ Formula/libcsptr.rb
@@ -4,18 +4,16 @@ $repo = "https://github.com/Snaipe/libcsptr.git"
 
 class Libcsptr < Formula
   homepage "https://github.com/Snaipe/libcsptr/"
-  url  $repo, :tag => "v2.0.2"
+  url  $repo, :tag => "v2.0.4"
   head $repo
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool"  => :build
-  depends_on "gcc"      => :build
-  fails_with :clang
+  depends_on "cmake" => :build
 
   def install
-    system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}"
+    system "cmake",
+        "-DCMAKE_BUILD_TYPE=RelWithDebInfo",
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+        "."
     system "make install"
   end
 end
